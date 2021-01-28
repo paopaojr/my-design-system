2bd476f8df0befb19f755de18aad48e04490d4db
paopaojr <paopaojr@gmail.com>
2021-01-28 15:23:39 +0700

    update

--- bin/check.ts
+++ bin/check.ts
@@ -4,31 +4,35 @@ import { promisify } from "util";
 const execAsync = promisify(exec);
 
 async function run() {
-  const output = await execAsync(
-    "yarn dlx -q lerna ls --since origin/master --json --loglevel=silent"
-  );
-  const changedPackages = JSON.parse(output.stdout);
+  try {
+    const output = await execAsync(
+      "yarn dlx -q lerna ls --since origin/master --json --loglevel=silent"
+    );
+    const changedPackages = JSON.parse(output.stdout);
 
-  let errors = [];
+    let errors = [];
 
-  for (const changed of changedPackages) {
-    const { name, version } = changed;
+    for (const changed of changedPackages) {
+      const { name, version } = changed;
 
-    const output = await execAsync(`npm show ${name} version`);
-    if (version.trim() === output.stdout.trim()) {
-      errors.push(
-        `Package ${name} code changed but version isn't bumped (local - ${version}, remote - ${output.stdout})`
-      );
+      const output = await execAsync(`npm show ${name} version`);
+      if (version.trim() === output.stdout.trim()) {
+        errors.push(
+          `Package ${name} code changed but version isn't bumped (local - ${version}, remote - ${output.stdout})`
+        );
+      }
     }
-  }
 
-  if (errors.length) {
-    console.error(errors.join("\n"));
+    if (errors.length) {
+      console.error(errors.join("\n"));
 
+      process.exit(1);
+    }
+
+    process.exit(0);
+  } catch (error) {
     process.exit(1);
   }
-
-  process.exit(0);
 }
 
 run();
